3cc7fdfe68fa4e39e6bdb1f291041ed2f22c33fe
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-11-07 10:27:05 -0500

    Run npm install before building

--- npm.sh
+++ npm.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 build() {
+    printf "\nInstalling npm packages...\n"
+    npm install
+
+    printf "\nRunning Build...\n"
     npm run-script build
+    
+    printf "\nStarting App...\n"
     npm start
 }
