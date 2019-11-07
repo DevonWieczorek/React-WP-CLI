4a17547502504a50d88c8bfabb77cdc749514fe2
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-11-07 11:02:30 -0500

    Added remote origin for React project

--- clone-repo.sh
+++ clone-repo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 fetchRepo() {
     git init
-    git remote add origin <REPO_LOCATION>
+    git remote add origin https://github.com/FluentCo/React-Wordpress-Microsite.git
     git pull origin master
     git add .
     git commit -a -m "Initial Commit"
