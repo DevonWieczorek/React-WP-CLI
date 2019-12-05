08f7b003a01e94ca37ca4b003328eff93d3f62e2
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-12-05 11:46:10 -0500

    Implemented try/catch for updating package.json

--- set-publish.js
+++ set-publish.js
@@ -3,8 +3,15 @@ const fs = require('fs');
 
 let raw = fs.readFileSync('package.json');
 let package = JSON.parse(raw);
-let cmd = package.scripts.publish;
 let regex = /(s3:\/\/)(.*?)(\/)(.*?)(\/)/gi;
 
-package.scripts.publish = cmd.replace(regex, `s3://${process.argv[2]}/${process.argv[3]}/`);
+try{
+    let _pub = package.scripts.publish;
+    package.scripts.publish = _pub.replace(regex, `s3://${process.argv[2]}/${process.argv[3]}/`);
+}
+catch(e){
+    let cmd = "cd $(npm root) && cd ../build && aws s3 cp $(pwd) s3://tsw-microsites-bucket/making-better-money/ --recursive";
+    package.scripts.publish = cmd.replace(regex, `s3://${process.argv[2]}/${process.argv[3]}/`);
+}
+
 fs.writeFileSync('package.json', JSON.stringify(package));
