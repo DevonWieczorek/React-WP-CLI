12d2165f5f0f0875d789e85c93988c5f9474d830
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-12-23 13:21:25 -0500

    Restructure and bug fix

--- types.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-module.exports = {
-    APP_NAME: 'react-wp',
-    APP_VERSION: '1.0.0',
-    APP_DESCRIPTION: 'CLI for updating .env files to configure new headless Wordpress sites'
-}
